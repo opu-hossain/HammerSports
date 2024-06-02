c3b7a464953019964c9afc1d244f7cae8e9796cf
opu-hossain <opu.hossain.data@gmail.com>
2024-06-02 09:13:57 +0600

    Initialized tailwind and scss for styling

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html', './Blog/templates/**/*.html', './authentication/templates/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
